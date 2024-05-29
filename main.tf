1ecaa39f34aabf847c99e42a20b8ed9d48f3b1cd
Florian de Vries <florian.devries@outlook.com>
2024-05-29 23:32:58 +0200

    Refactor billing_scope_id assignment in main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_subscription" "subscription" {
   subscription_name = var.subscription_name
   alias             = var.alias
-  billing_scope_id  = lookup(local.billing_scope_ids, keys(var.subscription_type)[0], var.billing_scope_id)
+  billing_scope_id  = var.subscription_id == null ? lookup(local.billing_scope_ids, keys(var.subscription_type)[0], var.billing_scope_id) : null
   subscription_id   = var.subscription_id
   workload          = var.workload
   tags              = var.tags
