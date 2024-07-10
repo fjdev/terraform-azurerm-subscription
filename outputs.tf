0e97b7c1b81fabc8f8caca3de998831043de73ee
Florian de Vries <florian.de.vries@conclusion.nl>
2024-07-10 09:49:59 +0200

    Update azurerm_subscription data source references

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,10 @@ output "subscription_id" {
 
 output "subscription_name" {
   description = "The Name of the Alias."
-  value       = azurerm_subscription.subscription.subscription_name
+  value       = azurerm_subscription.sub.subscription_name
 }
 
 output "tenant_id" {
   description = "The ID of the Tenant to which the subscription belongs."
-  value       = azurerm_subscription.subscription.tenant_id
+  value       = azurerm_subscription.sub.tenant_id
 }
