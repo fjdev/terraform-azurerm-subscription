74a0e73126435cf81de11e404bcd87c89047fd3d
Florian de Vries <florian.de.vries@conclusion.nl>
2024-07-10 09:03:46 +0200

    Update outputs and variables for Azure subscription configuration

--- variables.tf
+++ variables.tf
@@ -1,37 +1,37 @@
 variable "subscription_name" {
   type        = string
-  default     = null
   description = "(Required) The Name of the Subscription. This is the Display Name in the portal."
+  default     = null
 }
 
 variable "alias" {
   type        = string
-  default     = null
   description = "(Optional) The Alias name for the subscription. Terraform will generate a new GUID if this is not supplied. Changing this forces a new Subscription to be created."
+  default     = null
 }
 
 variable "billing_scope_id" {
   type        = string
-  default     = null
   description = "(Optional) The Azure Billing Scope ID. Can be a Microsoft Customer Account Billing Scope ID, a Microsoft Partner Account Billing Scope ID or an Enrollment Billing Scope ID."
+  default     = null
 }
 
 variable "subscription_id" {
   type        = string
-  default     = null
   description = "(Optional) The ID of the Subscription. Changing this forces a new Subscription to be created."
+  default     = null
 }
 
 variable "workload" {
   type        = string
-  default     = null
   description = "(Optional) The workload type of the Subscription. Possible values are Production (default) and DevTest. Changing this forces a new Subscription to be created."
+  default     = null
 }
 
 variable "tags" {
   type        = any
-  default     = null
   description = "(Optional) A mapping of tags to assign to the Subscription."
+  default     = null
 }
 
 variable "subscription_type" {
@@ -42,6 +42,6 @@ variable "subscription_type" {
     invoice_section_name    = optional(string)
     customer_name           = optional(string)
   }))
-  default     = null
   description = "(Optional) The Subscription Type to use and the required parameters for that Subscription Type. Possible values are 'EA', 'MCA' and 'MPA'."
+  default     = null
 }
