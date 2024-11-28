6b45e3587a9c4bf4e716767398ce33ea431bc16b
Florian de Vries <florian.de.vries@conclusion.nl>
2024-11-28 17:27:59 +0100

    Update outputs.tf to include Alias ID output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "id" {
+  description = "The ID of the Alias."
+  value       = azurerm_subscription.subscription.id
+}
+
 output "subscription_id" {
   description = "The Resource ID of the Alias."
   value       = data.azurerm_subscriptions.subscription.subscriptions[0].subscription_id
