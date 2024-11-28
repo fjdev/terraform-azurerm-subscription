c1ba0b1fed0db27e0b6e4e332378768112f0050e
Florian de Vries <florian.de.vries@conclusion.nl>
2024-11-28 17:17:16 +0100

    Update subscription_id and management group subscription association references

--- main.tf
+++ main.tf
@@ -8,8 +8,6 @@ resource "azurerm_subscription" "subscription" {
 }
 
 resource "azurerm_management_group_subscription_association" "management_group_subscription_association" {
-  count = var.management_group_id != null ? 1 : 0
-
   management_group_id = var.management_group_id
   subscription_id     = data.azurerm_subscriptions.subscription.subscriptions[0].id
 }
